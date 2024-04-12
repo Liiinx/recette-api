9ad437ce8df5ea2458c8517560da19766a495150
felix <felix@plan-immobilier.fr>
2024-04-12 12:27:17 +0200

    add all entity, execute database migration

--- src/Entity/Recipe.php
+++ src/Entity/Recipe.php
@@ -32,9 +32,25 @@ class Recipe
     #[ORM\OneToMany(mappedBy: 'recipe', targetEntity: Step::class, orphanRemoval: true)]
     private Collection $steps;
 
+    #[ORM\ManyToMany(targetEntity: Source::class, mappedBy: 'recipes')]
+    private Collection $sources;
+
+    #[ORM\OneToMany(mappedBy: 'recipe', targetEntity: Image::class, orphanRemoval: true)]
+    private Collection $images;
+
+    #[ORM\ManyToMany(targetEntity: Tag::class, mappedBy: 'recipes')]
+    private Collection $tags;
+
+    #[ORM\OneToMany(mappedBy: 'recipe', targetEntity: RecipeHasIngredient::class)]
+    private Collection $recipeHasIngredients;
+
     public function __construct()
     {
         $this->steps = new ArrayCollection();
+        $this->sources = new ArrayCollection();
+        $this->images = new ArrayCollection();
+        $this->tags = new ArrayCollection();
+        $this->recipeHasIngredients = new ArrayCollection();
     }
 
     public function isDraft(): ?bool
@@ -97,6 +113,120 @@ class Recipe
             // set the owning side to null (unless already changed)
             if ($step->getRecipe() === $this) {
                 $step->setRecipe(null);
+            }
+        }
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, Source>
+     */
+    public function getSources(): Collection
+    {
+        return $this->sources;
+    }
+
+    public function addSource(Source $source): static
+    {
+        if (!$this->sources->contains($source)) {
+            $this->sources->add($source);
+            $source->addRecipe($this);
+        }
+
+        return $this;
+    }
+
+    public function removeSource(Source $source): static
+    {
+        if ($this->sources->removeElement($source)) {
+            $source->removeRecipe($this);
+        }
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, Image>
+     */
+    public function getImages(): Collection
+    {
+        return $this->images;
+    }
+
+    public function addImage(Image $image): static
+    {
+        if (!$this->images->contains($image)) {
+            $this->images->add($image);
+            $image->setRecipe($this);
+        }
+
+        return $this;
+    }
+
+    public function removeImage(Image $image): static
+    {
+        if ($this->images->removeElement($image)) {
+            // set the owning side to null (unless already changed)
+            if ($image->getRecipe() === $this) {
+                $image->setRecipe(null);
+            }
+        }
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, Tag>
+     */
+    public function getTags(): Collection
+    {
+        return $this->tags;
+    }
+
+    public function addTag(Tag $tag): static
+    {
+        if (!$this->tags->contains($tag)) {
+            $this->tags->add($tag);
+            $tag->addRecipe($this);
+        }
+
+        return $this;
+    }
+
+    public function removeTag(Tag $tag): static
+    {
+        if ($this->tags->removeElement($tag)) {
+            $tag->removeRecipe($this);
+        }
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, RecipeHasIngredient>
+     */
+    public function getRecipeHasIngredients(): Collection
+    {
+        return $this->recipeHasIngredients;
+    }
+
+    public function addRecipeHasIngredient(RecipeHasIngredient $recipeHasIngredient): static
+    {
+        if (!$this->recipeHasIngredients->contains($recipeHasIngredient)) {
+            $this->recipeHasIngredients->add($recipeHasIngredient);
+            $recipeHasIngredient->setRecipe($this);
+        }
+
+        return $this;
+    }
+
+    public function removeRecipeHasIngredient(RecipeHasIngredient $recipeHasIngredient): static
+    {
+        if ($this->recipeHasIngredients->removeElement($recipeHasIngredient)) {
+            // set the owning side to null (unless already changed)
+            if ($recipeHasIngredient->getRecipe() === $this) {
+                $recipeHasIngredient->setRecipe(null);
             }
         }
 
